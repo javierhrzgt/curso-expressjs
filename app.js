c4fde24f0b1462eeb887f723276e63b4ba992cfc
javierhrzgt <eljavierhrz@gmail.com>
2025-11-19 20:00:58 -0600

    initial commit curso de Express.js

---
+++ app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+console.log(PORT);
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Curso Express.js v3</h1>
+    <p>Esto es una aplicación node.js con express.js<p>
+    <p>Corre en el puerto: ${PORT}</p>
+    `);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor: http://localhost:${PORT}`);
+});
