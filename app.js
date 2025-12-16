e69ac4082bea03ef4762e8023be8d32423b63665
javierhrzgt <eljavierhrz@gmail.com>
2025-12-15 18:39:57 -0600

    update

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 import bodyParser from "body-parser";
 import userRoutes from "./routes/userRoutes.js";
 import testRoutes from "./routes/testRoutes.js";
 import { LoggerMiddleware } from "./middlewares/logger.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
+const prisma = new PrismaClient();
 const app = express();
 
 // Middleware
@@ -19,6 +21,15 @@ const PORT = process.env.PORT || 3000;
 // Rutas
 app.use("/", testRoutes);
 app.use("/users", userRoutes);
+
+app.get("/db-users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users)
+  } catch (error) {
+    res.status(500).json({ error:"Error al comunicarse a la base de datos"})
+  }
+});
 
 // Servidor
 app.listen(PORT, () => {
